3e44b97a4e343e81df949a8828b5e4e8eb824d47
GUO YANKE <hi@yankeguo.com>
2025-04-11 15:25:00 +0800

    feat: add cert get or create

--- go.mod
+++ go.mod
@@ -3,18 +3,33 @@ module github.com/yankeguo/cert2cloud
 go 1.24.2
 
 require (
+	github.com/alibabacloud-go/cas-20200407/v3 v3.0.4
+	github.com/alibabacloud-go/darabonba-openapi/v2 v2.1.7
+	github.com/alibabacloud-go/tea v1.3.8
 	github.com/creasty/defaults v1.8.0
 	github.com/go-playground/validator/v10 v10.26.0
+	github.com/tjfoc/gmsm v1.4.1
 	github.com/yankeguo/rg v1.3.1
 )
 
 require (
+	github.com/alibabacloud-go/alibabacloud-gateway-spi v0.0.5 // indirect
+	github.com/alibabacloud-go/debug v1.0.1 // indirect
+	github.com/alibabacloud-go/endpoint-util v1.1.0 // indirect
+	github.com/alibabacloud-go/openapi-util v0.1.1 // indirect
+	github.com/alibabacloud-go/tea-utils/v2 v2.0.7 // indirect
+	github.com/aliyun/credentials-go v1.4.5 // indirect
+	github.com/clbanning/mxj/v2 v2.7.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	golang.org/x/crypto v0.33.0 // indirect
 	golang.org/x/net v0.34.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,22 +2,63 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"log"
+	"maps"
 	"os"
+	"slices"
+	"strings"
+	"time"
 
+	cas20200407 "github.com/alibabacloud-go/cas-20200407/v3/client"
+	openapi "github.com/alibabacloud-go/darabonba-openapi/v2/client"
+	"github.com/alibabacloud-go/tea/tea"
 	"github.com/creasty/defaults"
 	"github.com/go-playground/validator/v10"
+	"github.com/tjfoc/gmsm/x509"
 	"github.com/yankeguo/rg"
 )
 
 type Options struct {
 	CrtFile string `json:"crt_file" default:"/tls/tls.crt" validate:"required,file"`
 	KeyFile string `json:"key_file" default:"/tls/tls.key" validate:"required,file"`
-	Aliyun  struct {
-		AccessKeyId     string `json:"access_key_id" validate:"required"`
-		AccessKeySecret string `json:"access_key_secret" validate:"required"`
-		RegionId        string `json:"region_id" validate:"required"`
+	Aliyun  *struct {
+		AccessKeyId     string   `json:"access_key_id" validate:"required"`
+		AccessKeySecret string   `json:"access_key_secret" validate:"required"`
+		RegionId        string   `json:"region_id" validate:"required"`
+		CDNDomains      []string `json:"cdn_domains"`
 	} `json:"aliyun"`
+}
+
+func createAliyunCasClient(regionId, accessKeyId, accessKeySecret string) (*cas20200407.Client, error) {
+	config := &openapi.Config{
+		AccessKeyId:     tea.String(accessKeyId),
+		AccessKeySecret: tea.String(accessKeySecret),
+	}
+	config.Endpoint = tea.String("cas.aliyuncs.com")
+	return cas20200407.NewClient(config)
+}
+
+func nameFroCertificate(crt *x509.Certificate) string {
+	names := append([]string{crt.Subject.CommonName}, crt.DNSNames...)
+	var shortest string
+	for _, name := range names {
+		if shortest == "" || len(name) < len(shortest) {
+			shortest = name
+		}
+	}
+	prefixes := make(map[string]struct{})
+	for _, name := range names {
+		if strings.HasSuffix(name, shortest) {
+			name = strings.TrimSuffix(name, shortest)
+		}
+		if strings.HasPrefix(name, "*.") {
+			name = strings.TrimPrefix(name, "*.")
+		}
+		name = strings.ReplaceAll(name, ".", "-")
+		prefixes[name] = struct{}{}
+	}
+	return strings.Join(slices.Collect(maps.Keys(prefixes)), "-") + "-" + shortest
 }
 
 func main() {
@@ -36,4 +77,54 @@ func main() {
 	rg.Must0(json.Unmarshal(buf, &opts))
 	rg.Must0(defaults.Set(&opts))
 	rg.Must0(validator.New().Struct(opts))
+
+	crtPEM := rg.Must(os.ReadFile(opts.CrtFile))
+	keyPEM := rg.Must(os.ReadFile(opts.KeyFile))
+
+	crt := rg.Must(x509.ReadCertificateFromPem(crtPEM))
+
+	if time.Now().After(crt.NotAfter) {
+		err = errors.New("cert is expired")
+		return
+	}
+
+	if opts.Aliyun != nil {
+		casClient := rg.Must(createAliyunCasClient(opts.Aliyun.RegionId, opts.Aliyun.AccessKeyId, opts.Aliyun.AccessKeySecret))
+
+		var existingCrtList []*cas20200407.ListUserCertificateOrderResponseBodyCertificateOrderList
+		{
+			req := &cas20200407.ListUserCertificateOrderRequest{
+				OrderType: tea.String("UPLOAD"),
+			}
+			res := rg.Must(casClient.ListUserCertificateOrder(req))
+
+			existingCrtList = res.Body.CertificateOrderList
+		}
+
+		var crtId int64
+
+		for _, existingCrt := range existingCrtList {
+			if existingCrt.SerialNo != nil && *existingCrt.SerialNo == crt.SerialNumber.String() {
+				crtId = *existingCrt.CertificateId
+				log.Printf("certificate %d already exists, skip uploading", crtId)
+				goto crtFound
+			}
+		}
+
+		{
+			res := rg.Must(casClient.UploadUserCertificate(&cas20200407.UploadUserCertificateRequest{
+				Name: tea.String(nameFroCertificate(crt)),
+				Cert: tea.String(string(crtPEM)),
+				Key:  tea.String(string(keyPEM)),
+			}))
+
+			crtId = *res.Body.CertId
+			log.Printf("certificate %d uploaded, with dns names: %s", crtId, strings.Join(append([]string{crt.Subject.CommonName}, crt.DNSNames...), ", "))
+		}
+
+	crtFound:
+	} else {
+		err = errors.New("aliyun is not configured")
+	}
+
 }
